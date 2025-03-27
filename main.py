078e1602fbeef028fce941e4319ce4f32ed0cd51
Ochue <josueavila234@outlook.com>
2025-03-27 09:56:20 -0600

    Fix cosas

--- main.py
+++ main.py
@@ -8,15 +8,7 @@ import os
 app = FastAPI()
 
 # Habilitar CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://apiport.onrender.com"],
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["*"],
-    expose_headers=["*"],
-    max_age=600,
-)
+
 
 
 # Crear las tablas en la base de datos al iniciar la aplicación
