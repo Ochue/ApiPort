7fc48950bf5779aebc5fe9d6e933876fac23eb02
Ochue <josueavila234@outlook.com>
2025-03-27 13:50:23 -0600

    registro

--- models/user.py
+++ models/user.py
@@ -5,9 +5,9 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    full_name = Column(String, index=True)  # Cambié 'username' por 'full_name'
     email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)  # Asegúrate de que este campo exista
+    hashed_password = Column(String)  # Este campo debería coincidir con el nombre en el controlador
     token = Column(String(512), index=True, nullable=True)
 
     def __repr__(self):
