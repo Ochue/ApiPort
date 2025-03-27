45628c6ca21e8dc7236478168510635895b7d1a7
Ochue <josueavila234@outlook.com>
2025-03-27 13:22:09 -0600

    Cambios de seguiridad 4

--- models/user.py
+++ models/user.py
@@ -5,7 +5,10 @@ class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
-    password = Column(String)
-    token = Column(String, index=True)
+    full_name = Column(String(255), nullable=False, index=True)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    password = Column(String(255), nullable=False)
+    token = Column(String(512), index=True, nullable=True)  # Puede ser nulo si no ha iniciado sesión
+
+    def __repr__(self):
+        return f"<User(id={self.id}, full_name={self.full_name}, email={self.email})>"
